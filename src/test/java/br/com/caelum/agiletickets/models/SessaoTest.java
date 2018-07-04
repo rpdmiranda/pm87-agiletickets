7328fa4fecf3c5b055974291aaa8461b15b31602
Robson <paniago@senado.leg.br>
2018-07-04 09:31:19 -0300

    Correção no método para verificar se há vagas disponíveis.

--- src/test/java/br/com/caelum/agiletickets/models/SessaoTest.java
+++ src/test/java/br/com/caelum/agiletickets/models/SessaoTest.java
@@ -4,7 +4,15 @@ import org.junit.Assert;
 import org.junit.Test;
 
 public class SessaoTest {
-
+	
+	@Test
+	public void deveVender2IngressosSeHa2Vagas() {
+		Sessao sessao = new Sessao();
+		sessao.setTotalIngressos(2);
+		
+		Assert.assertTrue(sessao.podeReservar(2));
+	}
+	
 	@Test
 	public void deveVender1ingressoSeHa2vagas() throws Exception {
 		Sessao sessao = new Sessao();
