af0b857a4c1f55dc0913795124c7186ec61f0362
Robson <paniago@senado.leg.br>
2018-07-05 12:11:28 -0300

    Corrigido outro teste

--- src/test/java/br/com/caelum/agiletickets/models/SessaoTest.java
+++ src/test/java/br/com/caelum/agiletickets/models/SessaoTest.java
@@ -8,32 +8,31 @@ import org.junit.Assert;
 import org.junit.Test;
 
 public class SessaoTest {
-	
+
 	@Test
 	public void deveVender2IngressosSeHa2Vagas() {
 		Sessao sessao = new Sessao();
 		sessao.setTotalIngressos(2);
-		
+
 		Assert.assertTrue(sessao.podeReservar(2));
 	}
-	
+
 	@Test
 	public void naoDeveVenderNumeroInvalidoDeIngressos() {
 		Sessao sessao = new Sessao();
 		sessao.setTotalIngressos(2);
-		
+
 		Assert.assertFalse(sessao.podeReservar(0));
 		Assert.assertFalse(sessao.podeReservar(-10));
 		Assert.assertFalse(sessao.podeReservar(null));
 	}
-	
-	
+
 	@Test
 	public void deveVender1ingressoSeHa2vagas() throws Exception {
 		Sessao sessao = new Sessao();
-        sessao.setTotalIngressos(2);
+		sessao.setTotalIngressos(2);
 
-        Assert.assertTrue(sessao.podeReservar(1));
+		Assert.assertTrue(sessao.podeReservar(1));
 	}
 
 	@Test
@@ -45,94 +44,90 @@ public class SessaoTest {
 	}
 
 	@Test
-	public void reservarIngressosDeveDiminuirONumeroDeIngressosDisponiveis() throws Exception {
+	public void reservarIngressosDeveDiminuirONumeroDeIngressosDisponiveis()
+			throws Exception {
 		Sessao sessao = new Sessao();
 		sessao.setTotalIngressos(5);
 
 		sessao.reserva(3);
 		Assert.assertEquals(2, sessao.getIngressosDisponiveis().intValue());
 	}
-	
+
 	@Test
-	public void deveCriarApenasUmaSessaoQuandoAsDatasForemIguais() throws Exception {
+	public void deveCriarApenasUmaSessaoQuandoAsDatasForemIguais()
+			throws Exception {
 		Espetaculo espetaculo = new Espetaculo();
 		List<Sessao> sessoes = espetaculo.criaSessoes(
-				LocalDate.parse("2018-07-05"), 
-				LocalDate.parse("2018-07-05"), 
-				LocalTime.parse("22:00"),
-				Periodicidade.DIARIA);
-		
+				LocalDate.parse("2018-07-05"), LocalDate.parse("2018-07-05"),
+				LocalTime.parse("22:00"), Periodicidade.DIARIA);
+
 		Assert.assertNotNull(sessoes);
 		Assert.assertEquals("Apenas uma sessão", 1, sessoes.size());
 	}
-	
+
 	@Test
 	public void deveCriarDoisEspetaculos() throws Exception {
 		Espetaculo espetaculo = new Espetaculo();
 		List<Sessao> sessoes = espetaculo.criaSessoes(
-				LocalDate.parse("2018-07-05"), 
-				LocalDate.parse("2018-07-12"), 
-				LocalTime.parse("22:00"),
-				Periodicidade.SEMANAL);
-		
+				LocalDate.parse("2018-07-05"), LocalDate.parse("2018-07-12"),
+				LocalTime.parse("22:00"), Periodicidade.SEMANAL);
+
 		Assert.assertNotNull(sessoes);
 		Assert.assertEquals("Duas sessões", 2, sessoes.size());
-		Assert.assertEquals(new LocalDate(2018,7,5), sessoes.get(0).getInicio().toLocalDate());
-		Assert.assertEquals(new LocalDate(2018,7,12), sessoes.get(1).getInicio().toLocalDate());
+		Assert.assertEquals(new LocalDate(2018, 7, 5), sessoes.get(0)
+				.getInicio().toLocalDate());
+		Assert.assertEquals(new LocalDate(2018, 7, 12), sessoes.get(1)
+				.getInicio().toLocalDate());
 	}
-	
+
 	@Test
 	public void deveCriar8SessoesDiarias() {
 		Espetaculo espetaculo = new Espetaculo();
 		List<Sessao> sessoes = espetaculo.criaSessoes(
-				LocalDate.parse("2018-07-05"), 
-				LocalDate.parse("2018-07-12"), 
-				LocalTime.parse("22:00"),
-				Periodicidade.DIARIA);
-		
+				LocalDate.parse("2018-07-05"), LocalDate.parse("2018-07-12"),
+				LocalTime.parse("22:00"), Periodicidade.DIARIA);
+
 		Assert.assertNotNull(sessoes);
 		Assert.assertEquals("Duas sessões", 8, sessoes.size());
 	}
-	
+
 	@Test
 	public void naoDeveCriarDataFimMaiorDataInicioSemanal() throws Exception {
 		Espetaculo espetaculo = new Espetaculo();
 		List<Sessao> sessoes = espetaculo.criaSessoes(
-				LocalDate.parse("2018-07-12"), 
-				LocalDate.parse("2018-07-05"), 
-				LocalTime.parse("22:00"),
-				Periodicidade.SEMANAL);
-		
+				LocalDate.parse("2018-07-12"), LocalDate.parse("2018-07-05"),
+				LocalTime.parse("22:00"), Periodicidade.SEMANAL);
+
 		Assert.assertNotNull(sessoes);
 		Assert.assertEquals("Sem sessões criadas", 0, sessoes.size());
-//		Assert.assertEquals(new LocalDate(2018,7,5), sessoes.get(0).getInicio().toLocalDate());
-//		Assert.assertEquals(new LocalDate(2018,7,12), sessoes.get(1).getInicio().toLocalDate());
-	}	
-	
+		// Assert.assertEquals(new LocalDate(2018,7,5),
+		// sessoes.get(0).getInicio().toLocalDate());
+		// Assert.assertEquals(new LocalDate(2018,7,12),
+		// sessoes.get(1).getInicio().toLocalDate());
+	}
+
 	@Test
 	public void naoDeveCriarDataFimMaiorDataInicioDiario() throws Exception {
 		Espetaculo espetaculo = new Espetaculo();
 		List<Sessao> sessoes = espetaculo.criaSessoes(
-				LocalDate.parse("2018-07-12"), 
-				LocalDate.parse("2018-07-05"), 
-				LocalTime.parse("22:00"),
-				Periodicidade.DIARIA);
-		
+				LocalDate.parse("2018-07-12"), LocalDate.parse("2018-07-05"),
+				LocalTime.parse("22:00"), Periodicidade.DIARIA);
+
 		Assert.assertNotNull(sessoes);
 		Assert.assertEquals("Sem sessões criadas", 0, sessoes.size());
 	}
-	
+
 	@Test
 	public void naoDeveCriarSemDataInicioDiario() throws Exception {
+
 		Espetaculo espetaculo = new Espetaculo();
-		List<Sessao> sessoes = espetaculo.criaSessoes(
-				null, 
-				LocalDate.parse("2018-07-05"), 
-				LocalTime.parse("22:00"),
-				Periodicidade.DIARIA);
-		
-		Assert.assertNotNull(sessoes);
-		Assert.assertEquals("Sem sessões criadas", 0, sessoes.size());
-	}	
-	
+		try {
+			espetaculo.criaSessoes(null, LocalDate.parse("2018-07-05"),
+					LocalTime.parse("22:00"), Periodicidade.DIARIA);
+			Assert.fail("Não deveria criar ssessões sem data inicial");
+		} catch (RuntimeException e) {
+
+		}
+	}
+
 }
